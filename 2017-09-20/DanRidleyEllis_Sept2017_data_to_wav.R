36a6a9651c87da19555b5f509d6938177bc0b270
Caterina Constantinescu <CaterinaC@users.noreply.github.com>
2017-09-21 19:27:43 +0100

    Add files via upload

--- 2017-09-20/DanRidleyEllis_Sept2017_data_to_wav.R
+++ 2017-09-20/DanRidleyEllis_Sept2017_data_to_wav.R
@@ -1,1 +1,35 @@
+library(tuneR)
+library(seewave)
 
+  # the sample rate is effectively controlling the speed (frequency) of data playback 
+  samplef=8000 # sampling frequency in Hz
+  
+  # this controls the quality of sound.
+  samplebit=8 # sampling bit depth (8/16/24/32/64)
+
+  # grab some slightly appropriate data to use
+  amplitude = as.numeric(LakeHuron)
+  
+  # we need to set the mean value to zero
+  amplitude = amplitude - mean(amplitude)
+  
+  # make the wave object
+  Sound = Wave(left = amplitude, right = amplitude, samp.rate=samplef, bit=samplebit)
+  
+  # this is short, so let's just loop it a few times
+  
+  Sound = pastew(Sound, Sound, output = "Wave")
+  Sound = pastew(Sound, Sound, output = "Wave")
+  Sound = pastew(Sound, Sound, output = "Wave")
+  Sound = pastew(Sound, Sound, output = "Wave")
+  Sound = pastew(Sound, Sound, output = "Wave")
+  Sound = pastew(Sound, Sound, output = "Wave")
+
+  #Let's have a look at the sound
+  oscillo(Sound)
+  
+  # Play the sound  
+  listen(Sound)
+  
+  # You probably need to close your sound player before you can run this again
+  
